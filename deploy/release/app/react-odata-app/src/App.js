483f34e1492dccf456b9d918b9ac70a47b057457
Pascal van der Heiden <pascal.vanderheiden@outlook.com>
2023-05-31 13:32:22 +0200

    Update app.js

--- deploy/release/app/react-odata-app/src/App.js
+++ deploy/release/app/react-odata-app/src/App.js
@@ -1,62 +1,39 @@
-import { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   const [businessPartners, setBusinessPartners] = useState([]);
-  const [showBusinessPartners, setShowBusinessPartners] = useState(false);
+  const [showList, setShowList] = useState(false);
 
-  useEffect(() => {
-    async function fetchBusinessPartners() {
-      const response = await fetch('/api/sapbp/A_BusinessPartner?$top=10&$format=json');
-      const data = await response.json();
-      setBusinessPartners(data.d.results);
-    }
-    fetchBusinessPartners();
-  }, []);
+  const handleShowList = () => {
+    fetch('/api/sapbp/A_BusinessPartner?$top=10&$format=json')
+      .then(response => response.json())
+      .then(data => setBusinessPartners(data.d.results))
+      .catch(error => console.log(error));
+    setShowList(true);
+  };
 
-  function handleShowBusinessPartnersClick() {
-    setShowBusinessPartners(true);
-  }
-
-  function handleHideBusinessPartnersClick() {
-    setShowBusinessPartners(false);
-  }
+  const handleHideList = () => {
+    setShowList(false);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {!showBusinessPartners && (
-          <button onClick={handleShowBusinessPartnersClick}>
-            Show Business Partners
-          </button>
-        )}
-        {showBusinessPartners && (
-          <div>
-            <button onClick={handleHideBusinessPartnersClick}>
-              Hide Business Partners
-            </button>
-            <ul>
-              {businessPartners.map((bp) => (
-                <li key={bp.BusinessPartner}>
-                  {bp.BusinessPartner} - {bp.BusinessPartnerFullName}
-                </li>
-              ))}
-            </ul>
+        <h1>My Business Partners</h1>
+        {showList ? (
+          <div className="business-partners-grid">
+            {businessPartners.map(businessPartner => (
+              <div key={businessPartner.BusinessPartner} className="business-partner-card">
+                <div className="business-partner-name">{businessPartner.BusinessPartnerFullName}</div>
+                <div className="business-partner-id">{businessPartner.BusinessPartner}</div>
+              </div>
+            ))}
+            <button onClick={handleHideList}>Hide List</button>
           </div>
+        ) : (
+          <button onClick={handleShowList}>Show my Business Partners</button>
         )}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React, use ChatGPT
-        </a>
       </header>
     </div>
   );
